dfdebd8bfcc2e0336f6378e8d3ad231c70bb80dd
Sam Kohn <kohn@berkeley.edu>
2020-02-09 18:25:13 -0800

    Simplify the search functionality in find_tasks.py

--- find_tasks.py
+++ find_tasks.py
@@ -10,10 +10,6 @@ parser.add_argument('database')
 group = parser.add_mutually_exclusive_group()
 group.add_argument('--search',
         help='SQLite match string (WHERE Command LIKE <search>)')
-group.add_argument('--runfile', type=int, nargs=2,
-        help='Run and file numbers for a particular command')
-group.add_argument('--run', type=int, default=0,
-        help='All the commands with the given run number')
 group.add_argument('--all', action='store_true',
         help='All tasks')
 parser.add_argument('--taskid', action='store_true',
@@ -24,13 +20,13 @@ parser.add_argument('--time', action='store_true',
         help='Print the last updated Timestamp for matching tasks, one per line')
 args = parser.parse_args()
 if args.search is not None:
-    search = args.search
-elif args.run != 0:
-    search = '%{:07}%'.format(args.run)
+    search = ('%{}%'.format(args.search),)
+    where_clause_simple = 'WHERE Command LIKE ?'
+    where_clause_complex = 'AND Command LIKE ?'
 elif args.all:
-    search = '%'
-else:
-    search = '%{:07}%{:04}%'.format(*args.runfile)
+    search = ()
+    where_clause_simple = ''
+    where_clause_complex = ''
 
 toprint = []
 if args.taskid:
@@ -43,7 +39,7 @@ if args.time:
 with sqlite3.Connection(args.database) as conn:
     c = conn.cursor()
     if args.taskid and not args.status and not args.time:
-        c.execute('SELECT TaskID FROM tasks WHERE Command LIKE ?', (search,))
+        c.execute('SELECT TaskID FROM tasks' + where_clause_simple, search)
         for row in c.fetchall():
                 print(row[0])
     else:
@@ -55,9 +51,9 @@ with sqlite3.Connection(args.database) as conn:
                     ORDER BY Timestamp DESC) rn
                 FROM tasks JOIN history USING (TaskID)
             )
-            WHERE rn = 1
-            AND Command LIKE ?
-            ORDER BY TaskID''',
-            (search,))
+            WHERE rn = 1 ''' + where_clause_complex +
+
+            ''' ORDER BY TaskID''',
+            search)
         for row in c.fetchall():
             print(*[row[index] for index in toprint])
